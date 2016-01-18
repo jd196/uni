a703845d6ad1a6d462e2f297f5d5c19bfb362fbf
Jonathon Downey <P14137545@GWHXX583LIN018W.lec-admin.dmu.ac.uk>
2016-01-18 13:05:46 +0000

    addition

---
+++ calc.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+float add (float x, float y)
+{
+    return x + y;
+}
+
+float times (float x, float y)
+{
+	return x * y;
+}	
+
+float sub (float x, float y)
+{
+	return x - y;
+}	
+
+float div (float x, float y)
+{
+	return x / y;
+}	
+
+int main()
+{
+    float a, b, r;
+    char op;
+    do {
+       printf("number  op  number?  ");
+       scanf(" %f %c %f", &a, &op, &b);
+       switch (op)
+       {
+           case '+' : r = add(a,b);
+           break;
+           case '*' : r = times(a,b);
+           break;	   
+           case '/' : r = div(a,b);
+           break;	   
+           case '-' : r = sub(a,b);
+           	      break;
+           case 'q' : break;
+           default  : op='?';
+       }
+       if (op=='?')
+          printf("Unknown operator\n");
+       else if (op=='q')
+          printf("Bye\n");
+       else
+       	  printf("%f %c %f = %f\n", a, op, b, r);   
+  
+    }
+    while (op != 'q');
+    
+    return 0;
+}
+
